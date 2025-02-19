06e37accdbccf0eae106c4a8cd11b5e5261897ac
Rowel <razonabler23@gmail.com>
2025-02-19 00:28:35 -0800

    Added login endpoint

--- src/main/java/com/tracker/rest/api/controller/UserController.java
+++ src/main/java/com/tracker/rest/api/controller/UserController.java
@@ -1,16 +1,11 @@
 package com.tracker.rest.api.controller;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tracker.rest.api.model.User;
@@ -27,10 +22,32 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping()
-    public List<User> getAllUser() {
-        return userService.getAllUser();
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+
+        User loggedInUser = userService.loginAttempt(user);
+
+        if (loggedInUser != null) {
+            return ResponseEntity.ok(loggedInUser);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Account not matched");
+        }
     }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody User user) {
+        String response = userService.signupAttempt(user);
+
+        if (response.equals("Email already taken")) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(response); // 409 Conflict
+        }
+        return ResponseEntity.ok("Successfully signed up! Redirecting..."); // 200 OK
+    }
+
+    // @GetMapping()
+    // public List<User> getAllUser() {
+    // return userService.getAllUser();
+    // }
 
     // @PostMapping("/signup")
     // public String createAccount(@RequestParam String username, @RequestParam
@@ -40,19 +57,9 @@ public class UserController {
     // return userService.createAccount(username, password, email);
     // }
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody User user) {
-        String response = userService.signup(user);
-
-        if (response.equals("Email already taken")) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(response); // 409 Conflict
-        }
-        return ResponseEntity.ok("User signing up success..."); // 200 OK
-    }
-
-    @DeleteMapping("/deleteaccount")
-    public String deleteAccount(@RequestParam String username) {
-        return userService.deleteAccount(username);
-    }
+    // @DeleteMapping("/deleteaccount")
+    // public String deleteAccount(@RequestParam String username) {
+    // return userService.deleteAccount(username);
+    // }
 
 }
